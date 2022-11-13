7f1dac021e510544c303a4456187355a0107cfd8
Euler-37 <64597797+Euler-37@users.noreply.github.com>
2022-11-13 10:41:13 +0800

    Update check.f90

--- test/check.f90
+++ test/check.f90
@@ -1,8 +1,13 @@
 program check
    use linalg_fortran
    real(8)::a(3,3)
+   real(4)::b(3,3)
    call random_number(a)
+   b=a
    if(linalg%inv(a))then
       call linalg%print(a,"a")
+   end if
+   if(linalg%inv(b))then
+      call linalg%print(b,"b")
    end if
 end program check

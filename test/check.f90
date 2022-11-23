1870a84fe0310bdbbfbe0e6d890b02c9c74b99ed
Euler-37 <64597797+Euler-37@users.noreply.github.com>
2022-11-23 10:50:10 +0800

    Update check.f90

--- test/check.f90
+++ test/check.f90
@@ -1,7 +1,7 @@
 program check
    use linalg_fortran
-   real(8)::a(3,3)
-   real(4)::b(3,3)
+   real(8)::a(3,3),e(3),a8(3,3)
+   real(4)::b(3,3),w(3),b4(3,3)
    call random_number(a)
    b=a
    if(linalg%inv(a))then
@@ -10,4 +10,13 @@ program check
    if(linalg%inv(b))then
       call linalg%print(b,"b")
    end if
+   call linalg%gemm(a, a, a8, 'T', 'N', 1.d0, 0.d0)
+   call linalg%gemm(b, b, b4, 'T', 'N', 1.0 ,  0.0)
+   call linalg%eigh(a8,e)
+   call linalg%eigh(b4,w)
+   call linalg%print(e, "eigenvalue")
+   call linalg%print(a8, "eigenvactor")
+   call linalg%print(w, "eigenvalue")
+   call linalg%print(b4, "eigenvactor")
+
 end program check

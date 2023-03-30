387d3c566b3a6696e4f563fd5cc569d725a83180
Euler-37 <64597797+Euler-37@users.noreply.github.com>
2023-03-30 19:38:18 +0800

    Update check.f90

--- test/check.f90
+++ test/check.f90
@@ -1,22 +1,38 @@
-program check
+program main
    use linalg_fortran
-   real(8)::a(3,3),e(3),a8(3,3)
-   real(4)::b(3,3),w(3),b4(3,3)
+   implicit none
+   real(8)::a(3,3),c(3,3),d(3,3)
+   real(8)::e(3)
+   complex(8)::b(3)
    call random_number(a)
-   b=a
+   b=cmplx(a(:,1),a(:,2),8)
+   call linalg%print(a,"a")
+   call linalg%print(b,"b")
+   a=1.d0
+   c=2.d0
+   write(*,*)linalg%equal(a,c)
+   call linalg%eye(a)
+   call linalg%print(a,"a")
+   call random_number(a)
+   call linalg%print(a,"a")
    if(linalg%inv(a))then
-      call linalg%print(a,"a")
+      call linalg%print(a,"Inv a")
    end if
-   if(linalg%inv(b))then
-      call linalg%print(b,"b")
-   end if
-   call linalg%gemm(a, a, a8, 'T', 'N', 1.d0, 0.d0)
-   call linalg%gemm(b, b, b4, 'T', 'N', 1.0 ,  0.0)
-   call linalg%eigh(a8,e)
-   call linalg%eigh(b4,w)
-   call linalg%print(e, "eigenvalue")
-   call linalg%print(a8, "eigenvactor")
-   call linalg%print(w, "eigenvalue")
-   call linalg%print(b4, "eigenvactor")
+   call linalg%print(linalg%det(a),"Det (a)")
+   call random_number(a)
+   call random_number(c)
+   !call linalg%gemm(a,c,d,"N","N",1.d0,0.d0)
+   call linalg%gemm(a,c,d)
+   write(*,*)linalg%equal(d,matmul(a,c))
 
-end program check
+   call linalg%print(matmul(matmul(transpose(a),c),a),"A^T C A")
+   call linalg%geut(a,c)
+   call linalg%print(c,"A^T C A")
+   call random_number(a)
+   a=transpose(a)+a
+   c=a
+   call linalg%eigh(a,e)
+   call linalg%print(e,"EIG")
+   call linalg%geut(a,c)
+   call linalg%print(c,"B")
+end program main
